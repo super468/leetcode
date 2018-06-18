dbc57dfc32cf037d4f1dca72122c5c953c8a89f6
super468 <wtsbuct2013@163.com>
2018-06-18 10:38:19 +0800

    postorder analysis

--- src/BinaryTreePostorderTraversal.java
+++ src/BinaryTreePostorderTraversal.java
@@ -1,58 +1,61 @@
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Stack;
 
 /**
  * Creator : wts
  * Date : 6/17/18
  * Title : 145. Binary Tree Postorder Traversal
  * Description : Given a binary tree, return the postorder traversal of its nodes' values.
- * Analysis : to do
+ * Analysis :  http://www.cnblogs.com/AnnieKim/archive/2013/06/15/MorrisTraversal.html#3795455 这篇博客讲的很好
+ * 
+ * 但是作者对postorder的方法略复杂，postorder 和 preorder是类似的。
+ * preorder是中左右，把code中所有的left换成right，就成了中右左，然后再用addFirst倒序保存，也就是左右中
  */
 
 public class BinaryTreePostorderTraversal {
     public List<Integer> postorderTraversal(TreeNode root) {
-        List<Integer> list = new ArrayList<Integer>();
+        LinkedList<Integer> list = new LinkedList<Integer>();
         TreeNode current = root;
         while(current != null){
-            if(current.left == null){
-                current = current.right;
+            if(current.right != null){
+                TreeNode precede = current.right;
+                while(precede.left != null && precede.left != current){
+                    precede = precede.left;
+                }
+
+                if(precede.left == null){
+                    list.addFirst(current.val);
+                    precede.left = current;
+                    current = current.right;
+                }
+                else{
+                    precede.left = null;
+                    //list.add(current.val);
+                    current = current.left;
+                }
             }
             else{
-                TreeNode pre = current.left;
-                TreeNode tmp = pre;
-                while(pre.right != null && pre.right != current){
-                    if(pre.right == null){
-                        pre.right = current;
-                        current = current.left;
-                    }
-                    else{
-                        pre.right = null;
-                        reverse(tmp);
-                        TreeNode start = pre;
-                        while(start != null){
-                            list.add(start.val);
-                            start = start.right;
-                        }
-                        reverse(pre);
-                        current = current.right;
-                    }
-                }
+                list.addFirst(current.val);
+                current = current.left;
             }
         }
 
         return list;
     }
 
-    // reverse from the end to start
-    public TreeNode reverse(TreeNode start){
-        TreeNode pre = null;
-        TreeNode current = start;
-        while(current != null){
-            TreeNode p = current.right;
-            current.right = pre;
-            pre = current;
-            current = p;
+    public List<Integer> postorderTraversalIteration(TreeNode root) {
+        LinkedList<Integer> list = new LinkedList<Integer>();
+        Stack<TreeNode> stack = new Stack<>();
+        if(root == null) return list;
+        stack.push(root);
+        while(!stack.empty()){
+            TreeNode top = stack.pop();
+            if(top != null) list.addFirst(top.val); // 重要
+            if(top.left != null) stack.push(top.left);
+            if(top.right != null) stack.push(top.right);
         }
-        return pre;
+        return list;
     }
 }
