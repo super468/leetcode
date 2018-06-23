a3d41b679262c1388d83da64ed00e2c818d654f0
super468 <wtsbuct2013@163.com>
2018-06-23 22:20:54 +0800

    add iteration

--- src/BinaryTreeInorderTraversal.java
+++ src/BinaryTreeInorderTraversal.java
@@ -1,5 +1,7 @@
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Stack;
 
 /**
  * Creator : wts
@@ -42,5 +44,25 @@ public class BinaryTreeInorderTraversal {
         }
 
         return list;
+    }
+}
+
+class Iteration{
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> res = new LinkedList<Integer>();
+        if (root == null) return res;
+
+        Stack<TreeNode> stack = new Stack<TreeNode>();
+        TreeNode cur = root;
+        while (cur != null || !stack.isEmpty()) {
+            while (cur != null) {// Travel to each node's left child, till reach the left leaf
+                stack.push(cur);
+                cur = cur.left;
+            }
+            cur = stack.pop(); // Backtrack to higher level node A
+            res.add(cur.val);  // Add the node to the result list
+            cur = cur.right;   // Switch to A'right branch
+        }
+        return res;
     }
 }
